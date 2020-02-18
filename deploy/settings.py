8392665b415b922146dea364415a66efc9782468
lilue <aa_0933@qq.com>
2020-02-19 03:14:28 +0800

    1

--- deploy/settings.py
+++ deploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'effy%4ozg549#pz$@_rpk57589s42=#0m)s*d1c^8qaf8h839k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'deploy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
+        'PASSWORD': 'QQxuwen206420'
     }
 }
 
